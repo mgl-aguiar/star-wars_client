178aa0de36b7d2ed16ba2681ab876d07c7e99ab0
mferraris <mgl.ferraris@gmail.com>
2021-12-17 12:42:05 +0100

    changed comment and added readme file

--- src/store/characters/actions.js
+++ src/store/characters/actions.js
@@ -10,7 +10,8 @@ export const charactersFetched = (movieCharacters) => {
 
 export function fetchMovieCharacters(search, gender, sort) {
   return async (dispatch) => {
-    // had some help figuring out this logic to define the sorting method.
+    // had some help figuring out this logic to define the sorting method,
+    // since both sortByAge and SortByHeight shouldn't be queried at the same time.
     // probably changing the backend a bit would have made it simpler.
     const isSortByAge = sort.includes("age") ? sort : undefined;
     const sortByAge =
